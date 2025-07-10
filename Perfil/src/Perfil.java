e363b4d6706ad3a895fc7f93d8bf6755e353e20d
Nathali Herpich <nathaliherpichvalle@gmail.com>
2025-07-10 16:16:04 -0300

    Add files via upload

---
+++ Perfil/src/Perfil.java
@@ -0,0 +1,7 @@
+public class Perfil {
+    public static void main(String[] args) {
+        System.out.println("Olá,Nathali Herpich");
+        //moficação
+        System.out.println("How are you?");
+    }
+}
